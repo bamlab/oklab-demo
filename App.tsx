a79f6ec98f42fd620655101f1bf32705d900e8e2
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2023-11-27 16:31:27 +0100

    feat: add a RootNavigator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { MainPage } from './pages/MainPage';
+import { RootNavigator } from './components/RootNavigator';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <MainPage />
+        <RootNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
   );
---
+++ components/RootNavigator.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ColorPickerPage } from '../pages/ColorPickerPage';
+import { MainPage } from '../pages/MainPage';
+
+type RootStackParamList = {
+  Main: undefined;
+  ColorPicker: undefined;
+};
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+export const RootNavigator = () => (
+  <RootStack.Navigator
+    initialRouteName="Main"
+    screenOptions={{ headerShown: false }}
+  >
+    <RootStack.Screen name="Main" component={MainPage} />
+    <RootStack.Screen name="ColorPicker" component={ColorPickerPage} />
+  </RootStack.Navigator>
+);
